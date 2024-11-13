76318f1f8ed2619dd9c3db57bacf49f067e26117
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-11-13 22:44:59 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import { GraduationCap, Users, Calendar, Bell } from 'lucide-react';
+import logo from '../images/logo.png';
+
 
 function Navbar() {
     return (
@@ -8,10 +10,13 @@ function Navbar() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex items-center">
-                        <GraduationCap className="h-8 w-8 text-primary-600" />
+                        {/* <GraduationCap className="h-8 w-8 text-primary-600" />
                         <span className="ml-2 text-xl font-bold text-gray-900">
                             EDU Social
-                        </span>
+                        </span> */}
+                        <Link to="/">
+                            <img src={logo} alt="logo" className="h-20 w-20" />
+                        </Link>
                     </div>
                     <div className="flex items-center space-x-4">
                         <Link to="/">
