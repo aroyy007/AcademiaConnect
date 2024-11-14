da952ded22bd05deab6700bf630eb1353dc05a76
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-11-14 14:44:15 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import { GraduationCap, Users, Calendar, Bell } from 'lucide-react';
-import logo from '../images/logo.png';
-
+import { Link } from "react-router-dom";
+import { GraduationCap, Users, Calendar, Bell } from "lucide-react";
+import logo from "../images/logo.png";
 
 function Navbar() {
     return (
@@ -10,22 +9,22 @@ function Navbar() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex items-center">
-                        {/* <GraduationCap className="h-8 w-8 text-primary-600" />
-                        <span className="ml-2 text-xl font-bold text-gray-900">
-                            EDU Social
-                        </span> */}
                         <Link to="/">
                             <img src={logo} alt="logo" className="h-20 w-20" />
                         </Link>
                     </div>
-                    <div className="flex items-center space-x-4">
+                    <div className="flex items-center space-x-6"> 
                         <Link to="/">
-                            <button variant="outline" size="sm">
-                                Log in
+                            <button className="relative h-10 w-24 bg-transparent cursor-pointer border-2 border-gray-800 overflow-hidden rounded-full text-gray-800 transition-all duration-500 ease-in-out hover:bg-[#111827] hover:text-white">
+                                <span className="z-10 tracking-widest">Login</span>
+                                <span className="absolute inset-0 bg-gray-800 rounded-full transition-all duration-500 ease-in-out transform scale-0 hover:scale-[100]"></span>
                             </button>
                         </Link>
                         <Link to="/">
-                            <button size="sm">Sign up</button>
+                            <button className="relative h-10 w-24 bg-[#111827] text-white cursor-pointer border-2 border-transparent overflow-hidden rounded-full transition-all duration-500 ease-in-out hover:bg-white hover:text-[#111827]">
+                                <span className="z-10 tracking-widest">SignUp</span>
+                                <span className="absolute inset-0 bg-[#111827] rounded-full transition-all duration-500 ease-in-out transform scale-0 hover:scale-[100]"></span>
+                            </button>
                         </Link>
                     </div>
                 </div>
